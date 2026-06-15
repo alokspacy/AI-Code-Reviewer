f65795fa4f5a84332e8f8ade4674bac7315f6cc2
Alok Singh <alokaiml@gmail.com>
2026-06-15 14:25:51 +0530

    Add AI interaction routes and project updates

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,8 @@
-const app = require('./src/app');
+require('dotenv').config()
+const app = require('./src/app')
 
 
 
 app.listen(3000, () => {
-    console.log('server is running on http://localhost:3000');
+    console.log('server is running on http://localhost:3000')
 })
--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require('express')
 
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!')
 })
 
 
-module.exports = app;
+module.exports = app
