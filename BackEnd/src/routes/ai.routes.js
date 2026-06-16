6be13462baf2544ff163cc9d13ff049110d1fa18
Alok Singh <alokaiml@gmail.com>
2026-06-16 15:14:27 +0530

    Updated The AI Services

--- BackEnd/src/routes/ai.routes.js
+++ BackEnd/src/routes/ai.routes.js
@@ -1,16 +1,10 @@
 const express = require('express');
+const aiController = require("../controllers/ai.controller")
+
 const router = express.Router();
 
 
-router.get("/get-response", (req, res) => {
-
-    const prompt = req.query.prompt;
-
-    if (!prompt) {
-        return res.status(400).json({ error: "Prompt is required" });
-    }
-
-});
+router.post("/get-review", aiController.getReview)
 
 
-module.exports = router;
+module.exports = router;    
